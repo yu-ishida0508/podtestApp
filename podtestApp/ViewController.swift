d8ce96b28f209eb66fb1d8e0a69b263e4ab637d7
yu.i <vamos.ishida@icloud.com>
2020-04-29 15:43:00 +0900

    Initial Commit

---
+++ podtestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  podtestApp
+//
+//  Created by 石田悠 on 2020/04/29.
+//  Copyright © 2020 yuu.ishida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
